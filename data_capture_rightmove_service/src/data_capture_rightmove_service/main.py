278280f0fd6c7cddb38675d527abc5a8888d0907
daniel <danielnwachukwu5738@gmail.com>
2025-06-27 05:34:34 +0100

    Update: prepared data capture rightmove service for deployment. minor fix

--- data_capture_rightmove_service/src/data_capture_rightmove_service/main.py
+++ data_capture_rightmove_service/src/data_capture_rightmove_service/main.py
@@ -130,7 +130,7 @@ if __name__ == "__main__":
     import uvicorn
 
     uvicorn.run(
-        "src.data_capture_rightmove_service.main:app",
+        "data_capture_rightmove_service.main:app",
         host="0.0.0.0",
         port=8000,
         reload=settings.is_development(),
